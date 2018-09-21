673371ffb07099ba27a940ace6327852051cd2a4
mxc7609 <xm350@nyu.edu>
2018-09-21 09:29:25 +0800

    android

--- android/src/main/java/com/troila/alert/RNTroilaAlertModule.java
+++ android/src/main/java/com/troila/alert/RNTroilaAlertModule.java
@@ -4,19 +4,25 @@ package com.troila.alert;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.troila.customealert.CustomAlert;
 
 public class RNTroilaAlertModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNTroilaAlertModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNTroilaAlertModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNTroilaAlert";
-  }
+    @ReactMethod
+    public void showToast() {
+        CustomAlert customAlert=new CustomAlert(reactContext.getCurrentActivity());
+        customAlert.showToast("测试");
+    }
+
+    @Override
+    public String getName() {
+        return "RNTroilaAlert";
+    }
 }
